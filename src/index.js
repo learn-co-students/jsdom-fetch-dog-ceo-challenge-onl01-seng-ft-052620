1533af33ccfd3ad0dc1cb332b83bed555f5b16ed
Luca <lucakanyo@protonmail.com>
2020-09-04 03:39:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 let pics = () => document.querySelector("#dog-image-container")
 let list = () => document.querySelector("#dog-breeds")
+let dropdown = () => document.querySelector("#breed-dropdown")
 
+document.addEventListener("DOMContentLoaded", function(){
+    fetchPups()
+    fetchBreeds()
+});
+
+///////////////////////////////////////////////////////
 function fetchPups(){
     return fetch("https://dog.ceo/api/breeds/image/random/4")
   .then(resp => resp.json())
@@ -15,12 +22,16 @@ function usePups(pups){
     })
 }
 
+//////////////////////////////////////////////////////////////
 function fetchBreeds(){
     return fetch("https://dog.ceo/api/breeds/list/all")
     .then(resp => resp.json())
     .then(json => useBreeds(json))
 }
 
+
+
+//////////////////////////////////////////////////////////////
 function useBreeds(breeds){
 
     for (const breed in breeds.message) {
@@ -28,16 +39,38 @@ function useBreeds(breeds){
         li.innerHTML = breed 
         list().appendChild(li)
         changeColor(li)
+    }
 
-      }
+    function pupFilter(){
+        let choice = dropdown().value
+        list().innerHTML = ''
+        for (const breed in breeds.message) {
+            if (breed.startsWith(choice)){
+                const li = document.createElement('li')
+                li.innerHTML = breed 
+                list().appendChild(li)
+            }
+    }
+    }
+
+    dropdown().addEventListener("click", function(){
+        pupFilter()
+    })
+
 }
+///////////////////////////////////////////////////////////////
+
+
 
 let changeColor = li => li.addEventListener("click", function(){
     li.style.color = "deepskyblue"
 })
 
-document.addEventListener("DOMContentLoaded", function(){
-    fetchPups()
-    fetchBreeds()
-});
+
+
+
+
+
+
+
 
